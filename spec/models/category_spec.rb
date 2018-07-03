30b73f403c4eb043ab4f47e9adabf7e5d6e88194
Alexander Burton <amacburton@gmail.com>
2018-07-03 10:46:46 -0500

    added tests; enabled email to new users

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -2,6 +2,15 @@ require 'spec_helper'
 
 describe Category do
 
+  describe '#name' do
+    it 'should not allow blank' do
+      record = Category.new
+      record.name = nil
+
+      expect(record).to_not be_valid
+    end
+  end
+
   describe '#items' do
     it 'should add items' do
       c = Category.create(name: 'foo', description: 'bar')
@@ -10,7 +19,7 @@ describe Category do
       i3 = Item.create(name: 'foo', description: 'bar', category: c)
       i4 = Item.create(name: 'baz', description: 'qux', category: c)
 
-      c.items.size.should eq(4)
+      expect(c.items.size).to eq(4)
     end
   end 
 end
